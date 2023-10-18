63af01b7b363055b86bb72e46e25bd9de5140909
Mohammad Assad <assad62@hotmail.com>
2023-10-18 16:07:10 -0400

    Till end of lecture 11

--- api/index.js
+++ api/index.js
@@ -1,10 +1,28 @@
 import express from "express";
-import { dbConnection } from './database/connection.js';
-const app = express();
+import { dbConnection } from "./database/connection.js";
+import authRouter from "./routes/auth.route.js";
 
-dbConnection()
+const app = express();
+app.use(express.json());
+
+dbConnection();
+
+app.use("/api/auth", authRouter);
+
+
 
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
